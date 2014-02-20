d6379969cb5ed3ee573217a058b637de41870ff1
Kevin Crowell <Kevin.Crowell@pnnl.gov>
2014-02-20 20:00:11 +0000

    - Added some equality functionality to Peak

--- InformedProteomics.Backend/Data/Spectrometry/Peak.cs
+++ InformedProteomics.Backend/Data/Spectrometry/Peak.cs
@@ -18,6 +18,36 @@ namespace InformedProteomics.Backend.Data.Spectrometry
         {
 	        return Mz.CompareTo(other.Mz);
         }
+
+    	public bool Equals(Peak other)
+    	{
+    		if (ReferenceEquals(null, other)) return false;
+    		if (ReferenceEquals(this, other)) return true;
+			return Math.Abs(Mz - other.Mz) < 1e-9;
+    	}
+
+    	public override bool Equals(object obj)
+    	{
+    		if (ReferenceEquals(null, obj)) return false;
+    		if (ReferenceEquals(this, obj)) return true;
+    		if (obj.GetType() != typeof (Peak)) return false;
+    		return Equals((Peak) obj);
+    	}
+
+    	public override int GetHashCode()
+    	{
+    		return Mz.GetHashCode();
+    	}
+
+    	public static bool operator ==(Peak left, Peak right)
+    	{
+    		return Equals(left, right);
+    	}
+
+    	public static bool operator !=(Peak left, Peak right)
+    	{
+    		return !Equals(left, right);
+    	}
     }
 
     /// <summary>
