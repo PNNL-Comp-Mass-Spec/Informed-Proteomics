b33e6f680c259f6588f886563389d2c14b4d64d9
Josh Mendoza <joshua.mendoza@pnnl.gov>
2015-09-10 10:30:32 -0700

    Updated SequenceTagIndexFinder

--- InformedProteomics.Backend/SequenceTag/SequenceTagIndexFinder.cs
+++ InformedProteomics.Backend/SequenceTag/SequenceTagIndexFinder.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using InformedProteomics.Backend.Data.Biology;
+using InformedProteomics.Backend.Data.Composition;
 using InformedProteomics.Backend.Data.Sequence;
 using InformedProteomics.Backend.Data.Spectrometry;
 using InformedProteomics.Backend.Utils;
@@ -21,7 +24,7 @@ namespace InformedProteomics.Backend.SequenceTag
 
         }
 
-        public Tuple<int, int, int, int> GetLongestSequence(ProductSpectrum spectrum, Sequence sequence)
+        public Tuple<int, int, int, int, string, string> GetLongestSequence(ProductSpectrum spectrum, Sequence sequence)
         {
             _spectrum = spectrum;
             _sequence = sequence;
@@ -30,8 +33,19 @@ namespace InformedProteomics.Backend.SequenceTag
             var cleavages = _sequence.GetInternalCleavages().ToArray();
             var prefixValueArr = new int[cleavages.Length];
             var suffixValueArr = new int[cleavages.Length];
+            var prefixPeakArr = new Peak[cleavages.Length];
+            var suffixPeakArr = new Peak[cleavages.Length];
+            //var peakList = new double[_spectrum.Peaks.Length];
 
             int cleavageIndex = 0;
+
+            /*
+            for (int i = 0; i < peakList.Length; i++)
+            {
+                peakList[i] = _spectrum.Peaks[i].Intensity;
+            }*/
+
+            //var rankings = ArrayUtil.GetRankings(peakList);
 
             foreach (var c in cleavages)
             {
@@ -40,47 +54,82 @@ namespace InformedProteomics.Backend.SequenceTag
                     var fragmentComposition = baseIonType.IsPrefix
                                  ? c.PrefixComposition + baseIonType.OffsetComposition
                                  : c.SuffixComposition + baseIonType.OffsetComposition;
-                    for (int charge = _minCharge; charge <= _maxCharge; charge++)
+                    for (var charge = _minCharge; charge <= _maxCharge; charge++)
                     {
 
                         var ion = new Ion(fragmentComposition, charge);
-                        int baseIsotopePeakIndex;
-                        int nIsotopes;
-                        int nMatchedIsotopes;
+                        if (_spectrum.GetCorrScore(ion, _tolerance, RelativeIsotopeIntensityThreshold) < .7) continue;
+                        if (baseIonType.IsPrefix) prefixValueArr[cleavageIndex] = 1;
+                        else suffixValueArr[cleavageIndex] = 1;
 
-                        if (FindIon(ion, _tolerance, RelativeIsotopeIntensityThreshold, out baseIsotopePeakIndex, out nIsotopes, out nMatchedIsotopes))
-                        {
-                            if (baseIonType.IsPrefix) prefixValueArr[cleavageIndex] = 1;
-                            else suffixValueArr[cleavageIndex] = 1;
 
-                        }
                     }
                 }
                 cleavageIndex++;
             }
 
-            var sequenceArr = new int[_sequence.Count];
-            sequenceArr[0] = prefixValueArr[0];
-            sequenceArr[sequenceArr.Length - 1] = suffixValueArr[suffixValueArr.Length - 1];
+            var prefixSequenceArr = new int[_sequence.Count];
+            var suffixSequenceArr = new int[_sequence.Count];
+            prefixSequenceArr[0] = prefixValueArr[0];
+            suffixSequenceArr[suffixSequenceArr.Length - 1] = suffixValueArr[suffixValueArr.Length - 1];
 
-            /*for (int i = 1; i < prefixValueArr.Length ; i++)
+            for (int i = 1; i < prefixValueArr.Length; i++)
             {
-                if(prefixValueArr[i] == 1 && prefixValueArr[i - 1] == 1) sequenceArr[i] =  1;
+                if (prefixValueArr[i] == 1 && prefixValueArr[i - 1] == 1)
+                {
+                    if (_sequence[i] is ModifiedAminoAcid) continue;
+                    prefixSequenceArr[i] = 1;
+
+                }
             }
 
             for (int i = suffixValueArr.Length - 2; i >= 0; i--)
             {
-                if (suffixValueArr[i] == 1 && suffixValueArr[i + 1] == 1) sequenceArr[i + 1] = 1;
-            }*/
+                if (suffixValueArr[i] == 1 && suffixValueArr[i + 1] == 1)
+                {
+                    if (_sequence[i + 1] is ModifiedAminoAcid) continue;
+                    suffixSequenceArr[i + 1] = 1;
+                }
+            }
 
 
-            var prefixString = FindLongestSubstring(prefixValueArr);
-            var suffixString = FindLongestSubstring(suffixValueArr);
-            var prefixIndex = string.Concat(prefixValueArr);
-            var prefixStartIndex = prefixIndex.IndexOf(prefixString) + 1;
-            var suffixIndex = string.Concat(suffixValueArr);
-            var suffixStartIndex = suffixIndex.IndexOf(suffixString) + 1;
-            return new Tuple<int, int, int, int>(prefixStartIndex, prefixStartIndex + prefixString.Length - 1, suffixStartIndex, suffixStartIndex + suffixString.Length - 1);
+
+            var prefixSubString = FindLongestSubstring(prefixSequenceArr);
+            var prefixStartIndex = -1;
+            var prefixEndIndex = -1;
+            //var prefixSequencePeaks = new List<Peak>();
+            //var prefixPval = -1.0;
+            var prefixSequence = "";
+            if (prefixSubString != "")
+            {
+                var prefixIndex = string.Concat(prefixSequenceArr);
+                prefixStartIndex = prefixIndex.IndexOf(prefixSubString) + 1;
+                prefixEndIndex = (prefixStartIndex == 1) ? 1 : prefixStartIndex + prefixSubString.Length - 1;
+                //prefixSequencePeaks = GetPrefixSequencePeaks(prefixPeakArr, prefixStartIndex, prefixEndIndex);
+                //var prefixRankSum = GetSequenceRankSum(prefixSequencePeaks, rankings, peakList);
+                //prefixPval = FitScoreCalculator.GetRankSumPvalue(peakList.Length, prefixSequencePeaks.Count, prefixRankSum);
+                prefixSequence = GetStringSubSequence(_sequence, prefixStartIndex, prefixEndIndex);
+            }
+
+            var suffixSubString = FindLongestSubstring(suffixSequenceArr);
+            var suffixStartIndex = -1;
+            var suffixEndIndex = -1;
+            //var suffixSequencePeaks = new List<Peak>();
+            //var suffixPval = -1.0;
+            var suffixSequence = "";
+
+            if (suffixSubString != "")
+            {
+                var suffixIndex = string.Concat(suffixSequenceArr);
+                suffixStartIndex = suffixIndex.IndexOf(suffixSubString) + 1;
+                suffixEndIndex = (suffixStartIndex == 1) ? 1 : suffixStartIndex + suffixSubString.Length - 1;
+                //suffixSequencePeaks = GetSuffixSequencePeaks(suffixPeakArr, suffixStartIndex, suffixEndIndex);
+                //var suffixRankSum = GetSequenceRankSum(suffixSequencePeaks, rankings, peakList);
+                //suffixPval = FitScoreCalculator.GetRankSumPvalue(peakList.Length, suffixSequencePeaks.Count, suffixRankSum);
+                suffixSequence = GetStringSubSequence(_sequence, suffixStartIndex, suffixEndIndex);
+            }
+
+            return new Tuple<int, int, int, int, string, string>(prefixStartIndex, prefixEndIndex, suffixStartIndex, suffixEndIndex, prefixSequence, suffixSequence);
         }
 
 
@@ -96,14 +145,75 @@ namespace InformedProteomics.Backend.SequenceTag
         private Sequence _sequence;
 
 
+        /*private double GetSequenceRankSum(List<Peak> seqPeaks, int[] rankArr, double[] peakInt)
+        {
+            var rankSum = 0.0;
+            foreach (var p in seqPeaks)
+            {
+                var i = Array.IndexOf(peakInt, p.Intensity);
+                rankSum += rankArr[i];
+            }
+            return rankSum;
+        }*/
+
+        private string GetStringSubSequence(Sequence seq, int start, int end)
+        {
+            var startIndex = start - 1;
+            var endIndex = end - 1;
+            var subString = "";
+            if (startIndex == endIndex)
+            {
+                subString += seq[start].Residue;
+                return subString;
+            }
+            for (int i = startIndex; i < endIndex + 1; i++)
+            {
+                subString += seq[i].Residue;
+            }
+            return subString;
+        }
+
+        /*private List<Peak> GetPrefixSequencePeaks(Peak[] prefixPeaks, int start, int end)
+        {
+            var startIndex = start - 1;
+            var endIndex = end - 1;
+            var peaks = new List<Peak>();
+           
+            for (var i = startIndex; i <= endIndex; i++)
+            {
+                if (i > prefixPeaks.Length) continue;
+                if (i == startIndex && startIndex != 0)
+                {
+                    peaks.Add(prefixPeaks[i - 1]);
+                    peaks.Add(prefixPeaks[i]);
+                }
+                else peaks.Add(prefixPeaks[i]);
+            }
+            return peaks;
+        }*/
+
+        /*private List<Peak> GetSuffixSequencePeaks(Peak[] suffixPeaks, int start, int end)
+        {
+            var startIndex = start - 1;
+            var endIndex = end - 1;
+            var peaks = new List<Peak>();
+            
+            for (var i = startIndex; i <= endIndex; i++)
+            {
+                if (i > suffixPeaks.Length-1) continue;
+                if (i == startIndex && startIndex != 0)
+                {
+                    peaks.Add(suffixPeaks[i - 1]);
+                    peaks.Add(suffixPeaks[i]);
+                }
+                else peaks.Add(suffixPeaks[i]);
+            }
+            return peaks;
+        }*/
+
         private string FindLongestSubstring(int[] booleanSequence)
         {
-            var sequenceString = "";
-            foreach (var s in booleanSequence)
-            {
-                sequenceString += s;
-            }
-
+            var sequenceString = String.Concat(booleanSequence);
             Regex regex = new Regex(@"[1]+");
             var matches = regex.Matches(sequenceString);
             var largestSubstring = "";
@@ -112,93 +222,7 @@ namespace InformedProteomics.Backend.SequenceTag
                 if (m.Length > largestSubstring.Length) largestSubstring = m.Value;
             }
 
-
             return largestSubstring;
-        }
-
-        private bool FindIon(Ion ion, Tolerance tolerance, double relativeIntensityThreshold, out int baseIsotopePeakIndex, out int nIsotopes, out int nMatchedIsotopes)
-        {
-
-            var corrScore = _spectrum.GetCorrScore(ion, tolerance, relativeIntensityThreshold);
-
-            ///isotopePeaks
-            /// 
-            //FitScoreCalculator.GetPearsonCorrelation()
-            
-
-            
-            //matchedPeakIndex = new List<int>();
-            var baseIsotopeIndex = ion.Composition.GetMostAbundantIsotopeZeroBasedIndex();
-            var isotopomerEnvelope = ion.Composition.GetIsotopomerEnvelopeRelativeIntensities();
-            var baseIsotopMz = ion.GetIsotopeMz(baseIsotopeIndex);
-            baseIsotopePeakIndex = _spectrum.FindPeakIndex(baseIsotopMz, tolerance);
-
-            nIsotopes = isotopomerEnvelope.Select(x => x >= relativeIntensityThreshold).Count();
-            nMatchedIsotopes = 0;
-
-            if (baseIsotopePeakIndex < 0) return false;
-            //if (baseIsotopePeakIndex < 0) baseIsotopePeakIndex = ~baseIsotopePeakIndex;
-            nMatchedIsotopes++;
-
-            // go down
-            var peakIndex = baseIsotopePeakIndex;
-            //matchedPeakIndex.Add(peakIndex);
-            for (var isotopeIndex = baseIsotopeIndex - 1; isotopeIndex >= 0; isotopeIndex--)
-            {
-                if (isotopomerEnvelope[isotopeIndex] < relativeIntensityThreshold) break;
-
-                var isotopeMz = ion.GetIsotopeMz(isotopeIndex);
-                var tolTh = tolerance.GetToleranceAsTh(isotopeMz);
-                var minMz = isotopeMz - tolTh;
-                var maxMz = isotopeMz + tolTh;
-                for (var i = peakIndex - 1; i >= 0; i--)
-                {
-                    var peakMz = _spectrum.Peaks[i].Mz;
-                    if (peakMz < minMz)
-                    {
-                        //peakIndex = i;
-                        //break;
-                        return false;
-                    }
-                    if (peakMz <= maxMz)    // find match, move to prev isotope
-                    {
-                        peakIndex = i;
-                        //matchedPeakIndex.Add(peakIndex);
-                        nMatchedIsotopes++;
-                        break;
-                    }
-                }
-            }
-
-            // go up
-            peakIndex = baseIsotopePeakIndex;
-            for (var isotopeIndex = baseIsotopeIndex + 1; isotopeIndex < isotopomerEnvelope.Length; isotopeIndex++)
-            {
-                if (isotopomerEnvelope[isotopeIndex] < relativeIntensityThreshold) break;
-
-                var isotopeMz = ion.GetIsotopeMz(isotopeIndex);
-                var tolTh = tolerance.GetToleranceAsTh(isotopeMz);
-                var minMz = isotopeMz - tolTh;
-                var maxMz = isotopeMz + tolTh;
-                for (var i = peakIndex + 1; i < _spectrum.Peaks.Length; i++)
-                {
-                    var peakMz = _spectrum.Peaks[i].Mz;
-                    if (peakMz > maxMz)
-                    {
-                        //peakIndex = i;
-                        //break;
-                        return false;
-                    }
-                    if (peakMz >= minMz)    // find match, move to prev isotope
-                    {
-                        peakIndex = i;
-                        //matchedPeakIndex.Add(peakIndex);
-                        nMatchedIsotopes++;
-                        break;
-                    }
-                }
-            }
-            return true;
         }
 
         static SequenceTagIndexFinder()
