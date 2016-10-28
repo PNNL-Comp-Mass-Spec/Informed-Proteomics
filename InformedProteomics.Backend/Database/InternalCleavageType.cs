38fd93566536a62c596fceb32056d30f2a1fea10
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2016-10-28 13:41:58 -0700

    Move the tests into a solution folder
    Also, don't create an XML file for TopDownTrainer when in debug mode, and clean up invalid characters in a couple of XML comments

--- InformedProteomics.Backend/Database/InternalCleavageType.cs
+++ InformedProteomics.Backend/Database/InternalCleavageType.cs
@@ -6,7 +6,7 @@
         /// Any sequence in a protein that matches the parameters for N and C terminus cleavages
         /// </summary>
         /// <remarks>All cleavage combinations that meet the parameters for N and C terminus cleavage maximums will be checked.
-        /// NOTE: Any sequence minLength <= length <= maxLength</remarks>
+        /// NOTE: Any sequence minLength &lt;= length &lt;= maxLength</remarks>
         MultipleInternalCleavages = 0,
 
         /// <summary>
@@ -14,7 +14,7 @@
         /// </summary>
         /// <remarks>If the C terminus has no cleavage, the N terminus can have cleavages up to the specified N terminus cleavages max;
         /// if the N terminus has no cleavages, the C terminus can have cleavages up to the specified C terminus cleavages max.
-        /// NOTE: Any sequence minLength <= length <= maxLength, with C or N terminus cleavages</remarks>
+        /// NOTE: Any sequence minLength &lt;= length &lt;= maxLength, with C or N terminus cleavages</remarks>
         SingleInternalCleavage,
 
         /// <summary>
