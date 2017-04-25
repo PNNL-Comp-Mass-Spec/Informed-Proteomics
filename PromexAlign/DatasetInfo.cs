2dd0072d438848bc40c80fe85f4acd8485172079
wilkinscs <wilkinscs1@gmail.com>
2017-04-25 11:04:16 -0700

    PromexAlign Executable

---
+++ PromexAlign/DatasetInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace PromexAlign
+{
+    public class DatasetInfo
+    {
+        /// <summary>
+        /// Gets or sets the label for this dataset.
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path to the raw file.
+        /// </summary>
+        public string RawFilePath { get; set; }
+
+        /// <summary>
+        /// Gets the path to the ms1ft file.
+        /// </summary>
+        public string Ms1FtFilePath { get; set; }
+
+        /// <summary>
+        /// Gets the path to the MSPathFinder results file (_IcTda.tsv)
+        /// </summary>
+        public string MsPfIdFilePath { get; set; }
+
+        /// <summary>
+        /// Parse a dataset info file to get the datasets to run on.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static List<DatasetInfo> ParseDatasetInfoFile(string filePath)
+        {
+            var datasets = new List<DatasetInfo>();
+            foreach (var line in File.ReadLines(filePath))
+            {
+                var parts = line.Split('\t');
+                if (parts.Length < 3)
+                {
+                    continue;
+                }
+
+                string mspfIdFilePath = string.Empty;
+                if (parts.Length > 3)
+                {
+                    mspfIdFilePath = parts[3];
+                }
+
+                datasets.Add(new DatasetInfo
+                                 {
+                                     Label = parts[0],
+                                     RawFilePath = parts[1],
+                                     Ms1FtFilePath = parts[2],
+                                     MsPfIdFilePath = mspfIdFilePath
+                                 });
+            }
+
+            return datasets;
+        }
+    }
+}
