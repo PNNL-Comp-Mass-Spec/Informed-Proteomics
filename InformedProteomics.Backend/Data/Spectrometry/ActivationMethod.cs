5cac452441ed38393bf9973762907a8b07726a06
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2017-08-01 11:16:19 -0700

    Set explicit values for an enum to minimize possibility of breaking change

--- InformedProteomics.Backend/Data/Spectrometry/ActivationMethod.cs
+++ InformedProteomics.Backend/Data/Spectrometry/ActivationMethod.cs
@@ -8,36 +8,36 @@
         /// <summary>
         /// Collision-induced dissociation
         /// </summary>
-        CID,
+        CID = 0,
 
         /// <summary>
         /// Electron transfer dissociation
         /// </summary>
-        ETD,
+        ETD = 1,
 
         /// <summary>
         /// High-energy/beam-type collision induced dissociation
         /// </summary>
-        HCD,
+        HCD = 2,
 
         /// <summary>
         /// Electron capture dissociation
         /// </summary>
-        ECD,
+        ECD = 3,
 
         /// <summary>
         /// Pulsed q dissociation
         /// </summary>
-        PQD,
+        PQD = 4,
 
         /// <summary>
         /// Ultraviolet photo dissociation
         /// </summary>
-        UVPD,
+        UVPD = 5,
 
         /// <summary>
         /// Unknown activation method
         /// </summary>
-        Unknown,
+        Unknown = 6,
     }
 }
