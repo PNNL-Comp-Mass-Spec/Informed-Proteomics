0a0a59567a96ba6325411114457ecfa7af77cd03
alchemistmatt <matthew.monroe@pnnl.gov>
2017-11-13 17:14:15 -0800

    Fix mods file path

--- InformedProteomics.Tests.FunctionalTests/TestTopDownConsole.cs
+++ InformedProteomics.Tests.FunctionalTests/TestTopDownConsole.cs
@@ -18,7 +18,7 @@ namespace InformedProteomics.Tests.FunctionalTests
             var rawFilePath = Path.Combine(Utils.DEFAULT_TEST_FILE_FOLDER, @"TopDown\Lewy_ManyMods\Lewy_intact_01.raw");
             var featureFilePath = Path.Combine(Utils.DEFAULT_TEST_FILE_FOLDER, @"TopDown\Lewy_ManyMods\Lewy_intact_01.ms1ft");
             var databaseFilePath = Path.Combine(Utils.DEFAULT_TEST_FILE_FOLDER, @"TopDown\Lewy_ManyMods\ID_004858_0EE8CF61.fasta");
-            var modFilePath = Path.Combine(Utils.DEFAULT_TEST_FILE_FOLDER, @"TopDown\Lewy_ManyMods\Mods.txt");
+            var modFilePath = Path.Combine(Utils.DEFAULT_TEST_FILE_FOLDER, @"TopDown\Lewy_ManyMods\Lewy_DB_Mods.txt");
             var testOutputPath = Path.Combine(Utils.DEFAULT_TEST_FILE_FOLDER, @"TopDown\Lewy_ManyMods\TestOutput");
 
             var args = new[]
