594d3b34335dd451134f8d1be0aba79707b8c633
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2019-01-31 13:06:12 -0800

    Add a class to store peaks with detailed per-peak data; currently only adds noise, like what can be read from Thermo FT Centroid info.

---
+++ InformedProteomics.Backend/Data/Spectrometry/PeakDetailed.cs
@@ -0,0 +1,29 @@
+﻿namespace InformedProteomics.Backend.Data.Spectrometry
+{
+    /// <summary>
+    /// Like <see cref="Peak"/>, but with additional per-peak data, like the Noise value available from Thermo Label data.
+    /// </summary>
+    public class PeakDetailed : Peak
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mz"></param>
+        /// <param name="intensity"></param>
+        /// <param name="noise"></param>
+        public PeakDetailed(double mz, double intensity, double noise) : base(mz, intensity)
+        {
+            Noise = noise;
+        }
+
+        /// <summary>
+        /// Noise value for the peak
+        /// </summary>
+        public double Noise { get; }
+
+        /// <summary>
+        /// Calculated SignalToNoise value
+        /// </summary>
+        public double SignalToNoise => Intensity / Noise;
+    }
+}
