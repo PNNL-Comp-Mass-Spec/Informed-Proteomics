56227aa22a8f109006a77302432276e38c33745e
alchemistmatt <matthew.monroe@pnnl.gov>
2020-03-05 13:31:51 -0800

    Remove old file from distribution script

--- Executables/Update_Files.bat
+++ Executables/Update_Files.bat
@@ -6,7 +6,6 @@ xcopy ..\PbfGen\bin\Release\*.exe                                   PbfGen\ /D /
 xcopy ..\PbfGen\bin\Release\*.dll                                   PbfGen\ /D /Y
 xcopy ..\PbfGen\bin\Release\*.pdb                                   PbfGen\ /D /Y
 xcopy ..\PbfGen\bin\Release\PbfGen.exe.config                       PbfGen\ /D /Y 
-xcopy ..\PbfGen\bin\Release\ThermoRawFileReader.xml                 PbfGen\ /D /Y 
 xcopy ..\PbfGen\bin\Release\InformedProteomics.Backend.xml          PbfGen\ /D /Y 
 
 xcopy ..\ProMex\bin\Release\*.exe                                   ProMex\ /D /Y
@@ -14,13 +13,11 @@ xcopy ..\ProMex\bin\Release\*.dll                                   ProMex\ /D /
 xcopy ..\ProMex\bin\Release\*.pdb                                   ProMex\ /D /Y
 xcopy ..\ProMex\bin\Release\InformedProteomics.Backend.xml          ProMex\ /D /Y  
 xcopy ..\ProMex\bin\Release\ProMex.exe.config                       ProMex\ /D /Y  
-xcopy ..\ProMex\bin\Release\ThermoRawFileReader.xml                 ProMex\ /D /Y  
 
 xcopy ..\MSPathFinderT\bin\Release\*.exe                            MSPathFinder\ /D /Y
 xcopy ..\MSPathFinderT\bin\Release\*.dll                            MSPathFinder\ /D /Y
 xcopy ..\MSPathFinderT\bin\Release\*.pdb                            MSPathFinder\ /D /Y
 xcopy ..\MSPathFinderT\bin\Release\MSPathFinderT.exe.config         MSPathFinder\ /D /Y
-xcopy ..\MSPathFinderT\bin\Release\ThermoRawFileReader.xml          MSPathFinder\ /D /Y
 xcopy ..\MSPathFinderT\bin\Release\InformedProteomics.Backend.xml   MSPathFinder\ /D /Y
 xcopy ..\MSPathFinderT\bin\Release\InformedProteomics.Backend.Database.xml MSPathFinder\ /D /Y
 
