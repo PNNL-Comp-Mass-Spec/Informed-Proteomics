182895eba5005ef03124bb47ee2c8604d5f91124
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-23 16:04:35 -0700

    Suppress warnings

--- InformedProteomics.Backend/GlobalSuppressions.cs
+++ InformedProteomics.Backend/GlobalSuppressions.cs
@@ -5,6 +5,9 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:InformedProteomics.Backend.Data.Spectrometry.MzComparerWithBinning.#ctor(System.Int32)")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:InformedProteomics.Backend.Data.Spectrometry.MzComparerWithBinning.GetTolerance~InformedProteomics.Backend.Data.Spectrometry.Tolerance")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~P:InformedProteomics.Backend.Data.Spectrometry.MzComparerWithBinning.Ppm")]
 [assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:InformedProteomics.Backend.Data.Spectrometry.AbstractFragmentScorer.FindMatchedPeaks(InformedProteomics.Backend.Data.Composition.Composition,System.Double,System.Double)~System.Collections.Generic.IEnumerable{InformedProteomics.Backend.Data.Spectrometry.DeconvolutedPeak}")]
 [assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:InformedProteomics.Backend.Data.Spectrometry.AbstractFragmentScorer.FindMostIntensePeak(InformedProteomics.Backend.Data.Composition.Composition,System.Double,System.Double,System.Int32@,System.Double@,System.Double@)~InformedProteomics.Backend.Data.Spectrometry.Peak[]")]
 [assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:InformedProteomics.Backend.Data.Spectrometry.AbstractFragmentScorer.GetMinMaxChargeRange(System.Double)~InformedProteomics.Backend.Utils.IntRange")]
