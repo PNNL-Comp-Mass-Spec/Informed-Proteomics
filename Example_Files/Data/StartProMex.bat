e60a5848acfbb7af39cc1540b1032ba4b79345e1
alchemistmatt <matthew.monroe@pnnl.gov>
2021-07-16 17:25:09 -0700

    Add example result files

---
+++ Example_Files/Data/StartProMex.bat
@@ -0,0 +1,5 @@
+..\..\Executables\ProMex\ProMex.exe -i QC_SHEW_Intact_100ng_Elite_Run8_190502121701.raw  -minCharge 2 -maxCharge 60 -minMass 2000 -maxMass 50000 -score n -csv n -maxThreads 0
+..\..\Executables\ProMex\ProMex.exe -i QC_SHEW_Intact_100ng_Elite_Run9_190502121701.raw  -minCharge 2 -maxCharge 60 -minMass 2000 -maxMass 50000 -score n -csv n -maxThreads 0
+..\..\Executables\ProMex\ProMex.exe -i QC_SHEW_Intact_100ng_Elite_Run10_190502121701.raw -minCharge 2 -maxCharge 60 -minMass 2000 -maxMass 50000 -score n -csv n -maxThreads 0
+
+pause
