a43e01e019815a57143f8455c285733d16a66c71
alchemistmatt <matthew.monroe@pnnl.gov>
2022-09-27 16:31:58 -0700

    Add support for N- or C-terminal mods using 0 or peptide_length + 1 as the mod position

--- InformedProteomics.Backend/SearchResults/IdentificationExtensions.cs
+++ InformedProteomics.Backend/SearchResults/IdentificationExtensions.cs
@@ -18,10 +18,24 @@ namespace InformedProteomics.Backend.SearchResults
         {
             var aminoAcidSet = new AminoAcidSet();
             var sequence = new Sequence(peptide.Sequence, aminoAcidSet);
+            var sequenceLength = sequence.Count;
 
             foreach (var mod in peptide.Mods)
             {
-                var seqIndex = Math.Max(0, mod.Key - 1);
+                // Mod.Key is the 1-based position of the mod in the peptide
+                // However, N-terminal mods will have mod.Key = 0 and C-terminal mods will have mod.Key = peptide_length + 1
+
+                // The following if statement assures that N- or C- terminal mods are associated with the first or last residue in the peptide, respectively
+
+                int seqIndex;
+
+                if (mod.Key < 1)
+                    seqIndex = 0;
+                else if (mod.Key >= sequenceLength)
+                    seqIndex = sequenceLength - 1;
+                else
+                    seqIndex = mod.Key - 1;
+
                 var mzidMod = mod.Value;
 
                 var modification = Modification.Get(mzidMod.Tag, mzidMod.Mass);
